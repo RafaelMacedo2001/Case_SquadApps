28c34ca0e6f54c3a14f742ff1b999c6301260795
RafaelMacedo2001 <rafa.fm2001@gmail.com>
2023-04-22 12:38:34 -0300

    add alarm devices through API

--- app/src/main/java/com/example/motivation/AddDeviceActivity.kt
+++ app/src/main/java/com/example/motivation/AddDeviceActivity.kt
@@ -64,27 +64,26 @@ class AddDeviceActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
 
-            // Cadastro de dispositivo de vídeo
             R.id.button_save_video -> {
-                // Obtem as informações do dispositivo a partir dos campos de texto
-                val name = findViewById<EditText>(R.id.edit_nomevideo).text.toString()
-                val serial = findViewById<EditText>(R.id.edit_nsvideo).text.toString()
-                val username = findViewById<EditText>(R.id.edit_usuariovideo).text.toString()
-                val password = findViewById<EditText>(R.id.edit_senhavideo).text.toString()
+                // Código para salvar o dispositivo de vídeo
+                val namevideo = findViewById<EditText>(R.id.edit_nomevideo).text.toString()
+                val serialvideo = findViewById<EditText>(R.id.edit_nsvideo).text.toString()
+                val usernamevideo = findViewById<EditText>(R.id.edit_usuariovideo).text.toString()
+                val passwordvideo = findViewById<EditText>(R.id.edit_senhavideo).text.toString()
 
-                if (name.isNotEmpty() && serial.isNotEmpty() && username.isNotEmpty() && password.isNotEmpty()) {
+                if (namevideo.isNotEmpty() && serialvideo.isNotEmpty() && usernamevideo.isNotEmpty() && passwordvideo.isNotEmpty()) {
 
                     // Cria um objeto JSON com as informações do dispositivo
-                    val dispositivo = JSONObject()
-                        .put("name", name)
-                        .put("serial", serial)
-                        .put("username", username)
-                        .put("password", password)
+                    val dispositivovideo = JSONObject()
+                        .put("name", namevideo)
+                        .put("serial", serialvideo)
+                        .put("username", usernamevideo)
+                        .put("password", passwordvideo)
 
                     // Chama a função para criar o dispositivo na API
                     CoroutineScope(Dispatchers.IO).launch {
                         try {
-                            val response = criarDispositivo(dispositivo)
+                            val response = criarDispositivovideo(dispositivovideo)
                             withContext(Dispatchers.Main) {
                                 // Redireciona para a tela principal após a criação do dispositivo
                                 startActivity(
@@ -97,7 +96,7 @@ class AddDeviceActivity : AppCompatActivity(), View.OnClickListener {
                         } catch (e: Exception) {
                             withContext(Dispatchers.Main) {
                                 // Mostra a mensagem de erro ao usuário
-                                alert("Erro ao criar o dispositivo: ${e.message}")
+                                alertvideo("Erro ao criar o dispositivo: ${e.message}")
                             }
                         }
                     }
@@ -107,46 +106,124 @@ class AddDeviceActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }
             R.id.button_save_alarm -> {
-                // Implemente aqui o código para salvar o dispositivo de alarme
+
+                // Código para salvar o dispositivo de alarme
+                val namealarm = findViewById<EditText>(R.id.edit_nomealarme).text.toString()
+                val macalarm = findViewById<EditText>(R.id.edit_MAC).text.toString()
+                val passwordalarm = findViewById<EditText>(R.id.edit_senhaalarme).text.toString()
+
+                if (namealarm.isNotEmpty() && macalarm.isNotEmpty() && passwordalarm.isNotEmpty()) {
+
+                    // Cria um objeto JSON com as informações do dispositivo
+                    val dispositivoalarm = JSONObject()
+                        .put("name", namealarm)
+                        .put("macAddress", macalarm)
+                        .put("password", passwordalarm)
+
+                    // Chama a função para criar o dispositivo na API
+                    CoroutineScope(Dispatchers.IO).launch {
+                        try {
+                            val responsealarm = criarDispositivoalarm(dispositivoalarm)
+                            withContext(Dispatchers.Main) {
+                                // Redireciona para a tela principal após a criação do dispositivo
+                                startActivity(
+                                    Intent(
+                                        this@AddDeviceActivity,
+                                        MainActivity::class.java
+                                    )
+                                )
+                            }
+                        } catch (e: Exception) {
+                            withContext(Dispatchers.Main) {
+                                // Mostra a mensagem de erro ao usuário
+                                alertalarm("Erro ao criar o dispositivo: ${e.message}")
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Há campos a serem preenchidos!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
             }
         }
     }
 
-    // Função para criar um novo dispositivo na API
-    private fun criarDispositivo(dispositivo: JSONObject): String {
-        val API_BASE_URL = "http://squadapps.ddns-intelbras.com.br:3000"
-        val API_TOKEN =
+    // Função para criar um novo dispositivo de vídeo na API
+    private fun criarDispositivovideo(dispositivo: JSONObject): String {
+        val API_BASE_URL_video = "http://squadapps.ddns-intelbras.com.br:3000"
+        val API_TOKEN_video =
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjlhMmU0YTczLWNiODktNGUzZS1hMGE5LTYwODYxZDM3NWYwMSIsImlhdCI6MTY4MjAwODgzMSwiZXhwIjoxNjg0NjAwODMxfQ._WZkRusg8qj-kWeqVoFD3yVRdRneWmx7voHo2Jk7XU0"
 
-        val client = OkHttpClient()
+        val clientvideo = OkHttpClient()
 
         // Define o corpo da requisição com o objeto JSON do dispositivo
-        val body = dispositivo.toString().toRequestBody("application/json".toMediaType())
+        val bodyvideo = dispositivo.toString().toRequestBody("application/json".toMediaType())
 
         // Cria a requisição HTTP POST com o token de autenticação no header
-        val request = Request.Builder()
-            .url("$API_BASE_URL/video-devices")
-            .header("Authorization", "Bearer $API_TOKEN")
-            .post(body)
+        val requestvideo = Request.Builder()
+            .url("$API_BASE_URL_video/video-devices")
+            .header("Authorization", "Bearer $API_TOKEN_video")
+            .post(bodyvideo)
             .build()
+        Log.d("API_REQUEST", "Request: $requestvideo")
 
         // Envia a requisição e retorna o ID do dispositivo criado
-        val response = client.newCall(request).execute()
-        Log.d("API_RESPONSE", "Response: $response")
-        if (!response.isSuccessful) {
-            throw IOException("Erro ao criar o dispositivo: ${response.message}")
+        val responsevideo = clientvideo.newCall(requestvideo).execute()
+        Log.d("API_RESPONSE", "Response: $responsevideo")
+        if (!responsevideo.isSuccessful) {
+            throw IOException("Erro ao criar o dispositivo: ${responsevideo.message}")
         }
-        val responseBody = response.body?.string()
-        val responseJson = JSONObject(responseBody)
-        return responseJson.getString("id")
+        val responseBodyvideo = responsevideo.body?.string()
+        val responseJsonvideo = JSONObject(responseBodyvideo)
+        return responseJsonvideo.getString("id")
+    }
+    // Função para criar um novo dispositivo de alarme na API
+    private fun criarDispositivoalarm(dispositivo: JSONObject): String {
+        val API_BASE_URL_alarm = "http://squadapps.ddns-intelbras.com.br:3000"
+        val API_TOKEN_alarm =
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjlhMmU0YTczLWNiODktNGUzZS1hMGE5LTYwODYxZDM3NWYwMSIsImlhdCI6MTY4MjAwODgzMSwiZXhwIjoxNjg0NjAwODMxfQ._WZkRusg8qj-kWeqVoFD3yVRdRneWmx7voHo2Jk7XU0"
+
+        val clientalarm = OkHttpClient()
+
+        // Define o corpo da requisição com o objeto JSON do dispositivo
+        val bodyalarm = dispositivo.toString().toRequestBody("application/json".toMediaType())
+
+        // Cria a requisição HTTP POST com o token de autenticação no header
+        val requestalarm = Request.Builder()
+            .url("$API_BASE_URL_alarm/alarm-centrals")
+            .header("Authorization", "Bearer $API_TOKEN_alarm")
+            .post(bodyalarm)
+            .build()
+        Log.d("API_REQUEST", "Request: $requestalarm")
+
+        // Envia a requisição e retorna o ID do dispositivo criado
+        val responsealarm = clientalarm.newCall(requestalarm).execute()
+        Log.d("API_RESPONSE", "Response: $responsealarm")
+        if (!responsealarm.isSuccessful) {
+            throw IOException("Erro ao criar o dispositivo: ${responsealarm.message}")
+        }
+        val responseBodyalarm = responsealarm.body?.string()
+        val responseJsonalarm = JSONObject(responseBodyalarm)
+        return responseJsonalarm.getString("id")
+
     }
 
-    // Função para mostrar uma mensagem de alerta ao usuário
-    private fun alert(message: String) {
-        val builder = AlertDialog.Builder(this)
-        builder.setMessage(message)
+        // Função para mostrar uma mensagem de alerta video ao usuário
+    private fun alertvideo(message: String) {
+        val buildervideo = AlertDialog.Builder(this)
+        buildervideo.setMessage(message)
             .setPositiveButton("OK", null)
-        val alert = builder.create()
+        val alert = buildervideo.create()
         alert.show()
+    }
+
+    // Função para mostrar uma mensagem de alerta alarme ao usuário
+    private fun alertalarm(message: String) {
+        val builderalarm = AlertDialog.Builder(this)
+        builderalarm.setMessage(message)
+            .setPositiveButton("OK", null)
+        val alertalarm = builderalarm.create()
+        alertalarm.show()
     }
 }
